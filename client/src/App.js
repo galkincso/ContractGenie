fa064a995525460e2cd8bceaedc4362a68baea69
Kincső <galkincso9@gmail.com>
2024-02-28 12:02:21 +0100

    frontend
    First page

--- client/src/App.js
+++ client/src/App.js
@@ -3,35 +3,44 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 
 function App() {
 
   const [contracts, setContracts] = useState([]);
-
+  /*
   useEffect(() => {
     axios
-    .get('/findall')
-    .then(response => {
-      setContracts(response.data);
-      console.log(response);
-    })
+      .get('/findall')
+      .then(response => {
+        setContracts(response.data);
+        console.log(response);
+      })
 
   }, [])
+  */
 
   return (
     <>
-      <div>
-        <p>
-          Upload your contracts here.
-        </p>
+      <div className='header'>
+        <Box sx={{ width: '100%' }}>
+          <Typography variant="h1">
+            Contract Genie
+          </Typography>
+        </Box>
       </div>
-      <div>
-        <p>Contract patterns:</p>
-        {contracts.map((contract)=><p key={contract.id}>{contract.id}-{contract.name}</p>
-        )}
+      
+      <div className='options'>
+        <Button variant="contained" size='large'>Szerződések listázása</Button>
+        <Button variant="contained" size='large'>Szerződés feltöltése</Button>
       </div>
     </>
   );
 }
 
 export default App;
+
+// {contracts.map((contract) => <p key={contract.id}>{contract.id}-{contract.name}</p>)}
